4a7fb3fefda23a3c22a85522f09b58cb6e81d150
Carlos Catalan <103954507+Kuadc@users.noreply.github.com>
2022-04-30 13:10:39 -0300

    Update main.py

--- Ejercicio 5/main.py
+++ Ejercicio 5/main.py
@@ -1,1 +1,16 @@
+from ClaseLista import ListaPlan
+from ClaseMenu import Menu
+if __name__ == '__main__':
+    Lplan = ListaPlan()
+    Lplan.AbrirArchivo()
+    unmenu = Menu()
+    while True:
+        print('\n-------Menu--------')
+        print("1) - Actualizar el valor del vehículo de cada plan.")
+        print("2) - Mostrar código del plan, modelo y versión del vehículo.")
+        print("3) - Total monto para Licitar")
+        print("4) - Finalizar")
+        opcion= input("\nIngrese la opcion Deseada: ")
+        unmenu.opcion(opcion,Lplan)
+
 
